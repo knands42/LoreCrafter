f4800b27932a5c30cb235458fbfecfa22c1a410c
Caio Fernandes <caiow.wk2@proton.me>
2025-06-16 02:34:37 -0300

    fix: remove frontend

--- frontend/lib/verifyToken.ts
+++
@@ -1,9 +0,0 @@
-import {UserToken} from "@/lib/types";
-
-export const verifyToken = (token: string): UserToken => {
-    try {
-        return JSON.parse(atob(token.split('.')[1]));
-    } catch (error) {
-        throw new Error(`Invalid token: ${error}`);
-    }
-}
